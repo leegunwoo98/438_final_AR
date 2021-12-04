d09f3f4b04a5aa48913eaa61e73284415574bc4d
joohokim0514 <joohokim@wustl.edu>
2021-12-04 16:11:15 -0600

    Initial Commit

---
+++ 438-Final/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  438-Final
+//
+//  Created by Jooho Kim on 12/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var name: String?
+    var theme: String?
+    
+    
+    @IBAction func startGame(_ sender: Any) {
+        
+        print("Starting Game")
+        
+        let arController = storyboard!.instantiateViewController(withIdentifier: "ArView") as! ARController
+
+        navigationController?.pushViewController(arController, animated: true)
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
